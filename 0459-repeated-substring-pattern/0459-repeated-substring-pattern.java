220a320f0860d59175d4811bb096bfe38d4a9d79
Yuri Lee <68184617+lee-code712@users.noreply.github.com>
2023-11-16 21:41:13 +0900

    Time: 75 ms (34.92%), Space: 44 MB (52.16%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
         String sub = s + s;
-        sub = sub.substring(1, sub.length() - 1);
+        sub = sub.substring(1, sub.length() - 1); // 반복을 확인하기 위해 앞, 뒤 한문자 제외
         return sub.contains(s);
     }
 }
