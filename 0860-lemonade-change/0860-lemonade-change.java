4a817fc8c956d3882016fdcdcbdfab9b832ff0bd
Yuri Lee <68184617+lee-code712@users.noreply.github.com>
2023-12-14 23:54:16 +0900

    Time: 3 ms (26.25%), Space: 56.6 MB (65.36%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int[] changes = new int[2];
+        for (int bill : bills) {
+            if (bill == 5) {
+                changes[0]++;
+            }
+            else if (bill == 10) {
+                if (changes[0] < 1) return false;
+                changes[1]++;
+                changes[0]--;
+            }
+            else if (bill == 20) {
+                if (changes[0] < 1 || changes[1] < 1) { 
+                    if (changes[0] < 3) return false;
+                }
+                if (changes[0] >= 1 && changes[1] >= 1) {
+                    changes[0]--;
+                    changes[1]--;
+                }
+                else if (changes[0] >= 3) {
+                    changes[0] -= 3;
+                }
+            }
+        }
+        return true;
+    }
+}
